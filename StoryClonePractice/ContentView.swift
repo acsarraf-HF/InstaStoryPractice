6cf8200ca32982948f3270ca8c06a292870903b5
Alan Sarraf <alan.sarraf@hellofresh.com>
2025-07-10 17:11:53 +0200

    Initial commit

---
+++ StoryClonePractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StoryClonePractice
+//
+//  Created by Alan Sarraf on 10.07.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StoryClonePractice/StoryClonePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryClonePracticeApp.swift
+//  StoryClonePractice
+//
+//  Created by Alan Sarraf on 10.07.25.
+//
+
+import SwiftUI
+
+@main
+struct StoryClonePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
